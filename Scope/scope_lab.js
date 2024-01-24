c1b3ff1e6b558955c3913a08e5297d8f1fc45aaf
luckyalade <luckyalade309@gmail.com>
2024-01-24 18:52:36 -0500

    message

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,15 +1,31 @@
 //  Global Scope
-var globalVar = "I'm a global varibale";
-let globalLet = "I'm a global veribale but scoped with let";
-const globalCost = "I'm a global constant";
+var globalVar = "I'm a global variable";
+let globalLet = "I'm a global variable but scoped with let";
+const globalConst = "I'm a global constant";
 
 {
     // Block Scope
     var blockVar = "I'm a block-scoped var";
-    let blocklet = "I'm a blocked-scoped let";
+    let blockLet = "I'm a blocked-scoped let";
     const blockConst = "I'm a blocked-scoped const";
 }
 // Global scope
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+// Block scope
+// console.log(blockVar);
+// console.log(blockLet);
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    var functionLet = "I'm a block-scoped let";
+    var functionConst = "I'm a block-scoped Const";
+}
+
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
