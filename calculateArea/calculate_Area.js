ee0b7f9700e64badd4d25aca46702b7e5b9db79d
luckyalade <luckyalade309@gmail.com>
2024-01-25 14:35:20 -0500

    added Grocery Tracker function

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,13 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+// Grocery Tracker Function
+function groceryTotalAmount() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById('totalAmount').innerText = `The total amount is: $${total}`;
+}
