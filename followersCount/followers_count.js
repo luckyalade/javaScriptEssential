fc7c0978424481fdcbfc92c88ab4eb63e30b6670
luckyalade <luckyalade309@gmail.com>
2024-01-25 20:07:19 -0500

    added Reset Counter function in followerCount Folder

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,8 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
+function resetCount() {
+    document.getElementById('countDisplay').innerHTML = 0;
+    alert("Followers count has been reset");
+}
