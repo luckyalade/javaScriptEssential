7d595ef4b711a8e68c3ec466d121a700e13b72d6
luckyalade <luckyalade309@gmail.com>
2024-01-26 12:55:38 -0500

    added user experience functionality to Product Survey Feature

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,10 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    
-    const submitButton = document.getElementById("submitBtn");
-    submitButton.onclick =  submitFeedback;
-    alert('Thank you for your valuable feedback');
+    const experience = document.getElementById('userExperience').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -18,9 +15,15 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userProductExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 }
+
+const submitButton = document.getElementById("submitBtn");
+submitButton.addEventListener("click", function() {
+    submitFeedback();
+    alert('Thank you for your valuable feedback');
+}) 
 
 
 document.addEventListener('keydown', function(event) {
